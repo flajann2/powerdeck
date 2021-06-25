c7f6409785e1bb11f50707296246dd54b68bd72b
Fred Mitchell <fred.mitchell@gmx.de>
2021-06-25 15:38:30 +0200

    the example code works

--- examples/test-hid.hs
+++ examples/test-hid.hs
@@ -1,11 +1,23 @@
 -- testing the hidapi
+{-# LANGUAGE DataKinds #-}
 module Main where
 
 import qualified System.HIDAPI as H
 -- Bus 001 Device 010: ID 0fd9:006c Elgato Systems GmbH Stream Deck XL
 
+ 
 main = do
-  devinfo <- H.enumerate 0x0fd9::H.VendorID 0x006c::H.ProductID 
-  mapM print devinfo
-  return ()
+  H.init
+  devinfo <- H.enumerate (Just hvendor) Nothing
+  devall <- H.enumerateAll
+
+  putStrLn ">> All things:"
+  mapM_ print devall
+
+  putStrLn ">> Just Egalto Streamdeck(s):"
+  mapM_ print devinfo
+  H.exit
   
+  where  
+    hvendor = 0x0fd9 :: H.VendorID
+

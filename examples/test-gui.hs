14129e4f542b13ef7cbb76353577d36ff4f5a0fe
Fred Mitchell <fred.mitchell@gmx.de>
2021-06-28 18:10:35 +0200

    gui test works

--- examples/test-gui.hs
+++ examples/test-gui.hs
@@ -1,8 +1,32 @@
 -- Test of GUI and Yampa FRP
 
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
 module Main where
 
 import Lib
 
+{- cabal:
+build-depends: base, haskell-gi-base, gi-gtk == 3.0.*
+-}
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
 main :: IO ()
-main = someFunc
+main = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [ #title := "Hi there" ]
+
+  on win #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [ #label := "Click me" ]
+
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+
+  #add win button
+
+  #showAll win
+
+  Gtk.main

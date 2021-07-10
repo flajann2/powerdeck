6bec67318d1529e2f6d13ca7b591ee1a1545526b
Fred Mitchell <fred.mitchell@gmx.de>
2021-07-10 09:50:11 +0200

    feature specifications

--- app/Streamdeck-ui.hs
+++ app/Streamdeck-ui.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Main where
+
+import           Control.Monad                  ( void )
+import           Data.Text                      ( Text, pack )
+import           GI.Gtk                         ( Button(..)
+                                                , Grid(..)
+                                                , Label(..)
+                                                , Window(..)
+                                                )
+import           GI.Gtk.Declarative
+import           GI.Gtk.Declarative.App.Simple
+import           GI.Gtk.Declarative.Container.Grid
 
 import Lib
 
--- examples/test-gui.hs
+++ examples/test-gui.hs
@@ -7,7 +7,7 @@
 module Main where
 
 import           Control.Monad                  ( void )
-import           Data.Text                      ( pack )
+import           Data.Text                      ( Text, pack )
 import           GI.Gtk                         ( Button(..)
                                                 , Grid(..)
                                                 , Label(..)
@@ -17,27 +17,36 @@ import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
 import           GI.Gtk.Declarative.Container.Grid
 
-data State =
-  State
+data State = State (Maybe Text)
 
 data Event =
-  Closed
+  Confirmed
+  | Cancelled
+  | Closed
+  | Apple
+  | Bananna
+  | Carrot
+  | Durian
 
 view' :: State -> AppView Window Event
-view' State =
+view' (State msg) =
   bin
       Window
-      [ #title := "Grid"
+      [ #title := "Test GUI"
       , on #deleteEvent (const (True, Closed))
-      , #widthRequest := 400
-      , #heightRequest := 300
+      , #widthRequest := 640
+      , #heightRequest := 400
       ]
     $ container
         Grid
-        [#rowSpacing := 4, #columnSpacing := 4, #margin := 4]
+        [ #rowSpacing := 5
+        , #columnSpacing := 5
+        , #margin := 2]
         [ GridChild
           { properties = defaultGridChildProperties { width = 3, height = 3 }
-          , child      = widget Button [#label := "A", #vexpand := True]
+          , child      = widget Button [#label := "Apple"
+                                       , #vexpand := True
+                                       , on #clicked Apple]
           }
         , GridChild
           { properties = defaultGridChildProperties { width      = 3
@@ -45,7 +54,11 @@ view' State =
                                                     , leftAttach = 3
                                                     }
           , child = widget Button
-                           [#label := "B", #hexpand := True, #vexpand := True]
+                           [#label := "Bananna"
+                           , #hexpand := True
+                           , #vexpand := True
+                           , on #clicked Bananna]
+          
           }
         , GridChild
           { properties = defaultGridChildProperties { width      = 2
@@ -54,7 +67,10 @@ view' State =
                                                     , topAttach  = 1
                                                     }
           , child = widget Button
-                           [#label := "C", #hexpand := True, #vexpand := True]
+                           [#label := "Carrot"
+                           , #hexpand := True
+                           , #vexpand := True
+                           , on #clicked Carrot]
           }
         , GridChild
           { properties = defaultGridChildProperties { width      = 1
@@ -62,16 +78,22 @@ view' State =
                                                     , leftAttach = 5
                                                     , topAttach  = 2
                                                     }
-          , child      = widget Button [#label := "D"]
+          , child      = widget Button [#label := "Durian"
+                                       , on #clicked Durian]
           }
         ]
 
 update' :: State -> Event -> Transition State Event
-update' State Closed = Exit
+-- update' State Closed = Exit
+update' _ Apple = Transition (State (Just "Apple.")) (pure Nothing)
+update' _ Bananna = Transition (State (Just "Bananna.")) (pure Nothing)
+update' _ Durian = Transition (State (Just "Durian.")) (pure Nothing)
+update' _ Carrot = Transition (State (Just "Carrot.")) (pure Nothing) 
+update' _ Closed = Exit
 
 main :: IO ()
 main = void $ run App { view         = view'
                       , update       = update'
                       , inputs       = []
-                      , initialState = State
+                      , initialState = State Nothing
                       }
